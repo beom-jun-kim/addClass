d8d707be609f6f3a03ef24541a5933237e04ee37
beom jun kim <gimb52043@gmail.com>
2021-06-10 03:56:36 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+$(function(){
+
+    var obj = document.querySelector('button');
+    
+    var $title = $('h1');
+    var $button = $('button');
+
+    var $title = $('h1');
+    var $button = $('button');
+    
+    $button.click(function(){
+        console.log()
+    })
+})
